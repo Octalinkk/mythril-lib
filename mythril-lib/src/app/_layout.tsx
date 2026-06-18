430b2c21eaa0944482b53c44ba36c6fbf423e4fe
Octalink <btfoctalink@gmail.com>
2026-06-18 21:57:03 +0200

    Before android run

--- mythril-lib/src/app/_layout.tsx
+++ mythril-lib/src/app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { SQLiteProvider } from 'expo-sqlite';
+import { initDatabase } from '@/db/DBManager';
+import { Suspense } from 'react';
+import { ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+    return (
+        <SQLiteProvider
+            databaseName="data.db"
+            onInit={initDatabase}
+            useSuspense
+        >
+            <Suspense fallback={<ActivityIndicator />}>
+                <Stack screenOptions={{ headerShown: false }} />
+            </Suspense>
+        </SQLiteProvider>
+    );
 }
